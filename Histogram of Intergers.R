bd282df7a99a2aa244553497a98219fa284ecaea
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-06-29 21:40:43 -0400

    Add files via upload

---
+++ Histogram of Intergers.R
@@ -0,0 +1,7 @@
+### Histogram of Integers ###
+values<-rpois(n = 1000, lambda = 1.70)
+hist(values, main = "Histogram", xlab = "Random numbers from a Poisson with mean 1.7",
+     col = terrain.colors(6))
+
+hist(values, breaks = (-0.5:8.5), main = "Histogram from Poisson", xlab = "Random numbers from a Poisson with mean 1.7",
+     col = terrain.colors(6))
